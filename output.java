3a1c63a33bed83d1e67206d1f2f8fa72eb211eac
Ram Songire <songireram@gmail.com>
2026-05-05 14:59:43 +0530

    Add files via upload
    Output In Java Programming

---
+++ output.java
@@ -0,0 +1,12 @@
+class output{
+    public static void main(String[] args) {
+        //Single line output
+        System.out.print("First Line");
+        System.out.println("New Line");
+
+
+        //New line output "Use ln to print output on next line"
+        System.out.println("First Line");
+        System.out.println("New Line");
+    }
+}
