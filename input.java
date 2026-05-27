e4b3eb29e1e98eb8fee3b59e3e50ba230df20b1f
Ram Songire <songireram@gmail.com>
2026-05-27 10:50:14 +0530

    Add files via upload
    Take Input from user

---
+++ input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class input {
+    public static void main(String[] args) {
+
+        System.out.println("Enter Your Name");
+        Scanner sc = new Scanner(System.in); // Scanner object for taking input from user
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
